852c8650004bbf2aa7a32c38fb7d6393f823e4e3
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-09-17 18:08:15 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sincos
+{
+    public partial class Form1 : Form
+    {
+        private Image i; Bitmap b; int value = 0; int value2 = 0; int xx = 0; int yy = 0; Graphics ee; int xxx = 0; int yyy = 0; int xxxx = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double [] dcos ={ 1.00,0.707,0.00,-0.707,-1.00,-0.707,0.00,0.707,1.00};
+            double[] dsin = { 0.00, 0.707, 1.00, 0.707, 0.00, -0.707, -1.00, -0.707, 0.0 };
+            b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = b;
+            xxx = pictureBox1.Width / 2;
+            yyy = pictureBox1.Height / 2;
+            ee = Graphics.FromImage(b);
+            Pen ppp = new Pen(Color.FromArgb(0, 0, 0));
+
+            for (int a = 0; a < 8; a++) ee.DrawLine(ppp, new Point((int)(dsin[a]*50.00)+50, (int)(dcos[a]*50.00)+50), new Point((int)(dsin[a + 1]*50.00)+50, (int)(dcos[a + 1]*50.00)+50));
+            ppp.Dispose();
+            
+            ee.Dispose();
+            
+        }
+    }
+}
